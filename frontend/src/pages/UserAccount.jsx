8e8d9b09ba650f3d6a6b78895a096ea77b42fe74
AlphaHandia <118402552+AlphaHandia@users.noreply.github.com>
2023-11-29 00:02:28 +0100

    rename

--- frontend/src/pages/UserAccount.jsx
+++ frontend/src/pages/UserAccount.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-import MainAccount from "../components/MainAccount/MainAccount";
+import MainAccount from "../components/UserWelcome/MainAccount";
 import Account from "../components/Account/Account";
 const User = () => {
     return (
