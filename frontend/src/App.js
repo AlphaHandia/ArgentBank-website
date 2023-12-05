5c6cd372856f7a2562a8e765414889e68f5b8e82
AlphaHandia <118402552+AlphaHandia@users.noreply.github.com>
2023-12-05 10:44:05 +0100

    ouverture sur page home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,12 +10,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/argentBank" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route element={<PrivateRoute />}>
           <Route path="/user-account" element={<UserAccount />} />
         </Route>
-        <Route path="*" element={<Error />} />
+        <Route path="/*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
